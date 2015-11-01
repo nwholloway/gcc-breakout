f3b0082a695f7149db9099c97e1fbd484d7ded96
Nick Holloway <Nick.Holloway@pyrites.org.uk>
2015-11-01 16:57:51 +0000

    Version 3
    Add bricks!

--- breakout.py
+++ breakout.py
@@ -10,6 +10,7 @@ def game_init():
     global arena
     global ball_image
     global paddle_image
+    global brick_images
 
     # Initialise pygame
     pygame.init()
@@ -26,6 +27,7 @@ def game_init():
     # Load images
     ball_image = pygame.image.load("ball.png")
     paddle_image = pygame.image.load("paddle.png")
+    brick_images = [pygame.image.load("brick" + str(i) + ".png") for i in [0, 1, 2, 3, 4]]
 
 
 def game_run():
@@ -47,14 +49,25 @@ def game_reset():
     global screen, arena
     global ball_image, ball_position, ball_direction
     global paddle_image, paddle_position
-
-    ball_position = ball_image.get_rect()
-    ball_position.center = (screen.get_width() / 2, 200)
-
-    ball_direction = [-1, 1]
+    global brick_images, brick_positions
 
     paddle_position = paddle_image.get_rect()
     paddle_position.bottomleft = (arena.left, arena.bottom - 10)
+
+    y = arena.top + 32
+    brick_positions = []
+    for i, brick_image in enumerate(brick_images):
+        brick_positions.append([])
+        brick_rect = brick_image.get_rect()
+        for x in range(arena.left, arena.right, brick_rect.width):
+            brick_position = brick_rect.move(x, y)
+            brick_positions[i].append(brick_position)
+        y += brick_rect.height
+
+    ball_position = ball_image.get_rect()
+    ball_position.midtop = (screen.get_width() / 2, y + 2)
+
+    ball_direction = [-1, 1]
 
 
 def game_input():
@@ -80,6 +93,7 @@ def game_update():
     global arena
     global ball_position, ball_direction
     global paddle_position
+    global brick_positions
 
     # Milliseconds used in previous tick
     tick = clock.get_time()
@@ -95,6 +109,13 @@ def game_update():
     if keys[pygame.K_RIGHT]:
         paddle_position.x += tick // 4
     paddle_position.clamp_ip(arena)
+
+    # If the ball hits a brick, remove it and bounce down
+    for brick_line in brick_positions:
+        for brick_position in brick_line:
+            if ball_position.colliderect(brick_position):
+                brick_line.remove(brick_position)
+                ball_direction[1] = 1
 
     # If the ball hits the paddle, bounce up
     if ball_position.colliderect(paddle_position):
@@ -123,6 +144,11 @@ def game_draw():
 
     # Draw the arena
     pygame.draw.rect(screen, [0, 0, 0], arena)
+
+    # Draw the bricks
+    for i, brick_image in enumerate(brick_images):
+        for brick_position in brick_positions[i]:
+            screen.blit(brick_image, brick_position)
 
     # Draw the paddle
     screen.blit(paddle_image, paddle_position)
